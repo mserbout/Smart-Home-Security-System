3ce19e6d0b1f0f45589ed944cb232a834091f3dc
predenz99 <predenz99@gmail.com>
2024-05-21 13:09:45 +0200

    update

--- sketch_may21b/sketch_may21b.ino
+++ sketch_may21b/sketch_may21b.ino
@@ -1,74 +1,49 @@
-#include <string>
 #include <ESP8266WiFi.h>
 #include "DHTesp.h"
 
+// Replace with your network credentials
+const char* ssid = "bra";
+const char* password = "00000000";
 
-WiFiClient wClient;
 DHTesp dht;
 
-// Update these with values suitable for your network.
-const String ssid = "master1";
-const String password = "mohamed11";
+void setup() {
+  // Start the serial communication to get information via serial monitor
+  Serial.begin(115200);
+  delay(10);
 
-// cadenas para topics e ID
-String ID_PLACA;
+  // Start the WiFi connection
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-
-// GPIOs
-int LED1 = 2;  
-int LED2 = 16; 
-
-//-----------------------------------------------------
-void conecta_wifi() {
-  Serial.println("Connecting to " + ssid);
- 
-  WiFi.mode(WIFI_STA);
+  // Begin WiFi connection
   WiFi.begin(ssid, password);
 
+  // Wait until the connection has been established
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED1,!digitalRead(LED1));
-    delay(200);
+    delay(500);
     Serial.print(".");
   }
-  digitalWrite(LED1,HIGH);
-  Serial.println();
-  Serial.println("WiFi connected, IP address: " + WiFi.localIP().toString());
-}
 
-
-
-
-void setup() {
-  Serial.begin(115200);
-  
-  Serial.println();
-  Serial.println("Empieza setup...");
-  
-  pinMode(LED1, OUTPUT);    // inicializa GPIO como salida
-  pinMode(LED2, OUTPUT);    
-  digitalWrite(LED1, LOW); 
-  digitalWrite(LED2, HIGH); // apaga el led
-  
-  // crea topics usando id de la placa
-  ID_PLACA="ESP_" + String( ESP.getChipId() );
-  conecta_wifi();
-  
-  
-  Serial.println("Identificador placa: "+ ID_PLACA);
-  Serial.println("Termina setup en " +  String(millis()) + " ms");
+  // Connection established
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
   dht.setup(5, DHTesp::DHT11); // Connect DHT sensor to GPIO 5
 }
 
-
 unsigned long ultimo_mensaje=0;
 
 void loop() {
-  
-  
+  // Put your main code here, to run repeatedly
+
   unsigned long ahora = millis();
 
-  if (ahora - ultimo_mensaje >= 10000) {
+  if (ahora - ultimo_mensaje >= 3000) {
     float humidity = dht.getHumidity();
     float temperature = dht.getTemperature();
     ultimo_mensaje = ahora;
@@ -76,7 +51,5 @@ void loop() {
     Serial.println();
     Serial.println("Payload : "+ mensaje);
     
-    
-    digitalWrite(LED2, LOW); // enciende el led al enviar mensaje
   }
 }
